dc95e1f2d0f001771096865fbb0418f52568a3c2
Ralf Mouthaan <rpm40@cam.ac.uk>
2024-05-19 14:39:32 +0930

    Proper boundary conditions in grad
    One boundary condition for edges. One boundary condition for the real / imaginary interface.

--- Functions/MyDiv3D.m
+++ Functions/MyDiv3D.m
@@ -1,18 +1,20 @@
 function y=MyDiv3D(TV)
 
-n=size(TV);
+% g = Grad(TV(:,:,1));
+% y = -g(:,:,1) - g(:,:,2);
+
+[nx, ny, nz] = size(TV);
 
 x = TV(:,:,1);
 x_shift = circshift(x, [1 0]);
 yx = x - x_shift;
-yx(1,:) = x(1,:);
-yx(n(1),:)= - x_shift(n(1),:);
-
+%yx(1,:) = x(1,:);
+yx(nx,:)= -x_shift(nx,:);
 
 y = TV(:,:,2);
 y_shift = circshift(y, [0 1]);
 yy = y - y_shift;
-yy(:,1) = y(:,1);
-yy(:,n(2)) = -y_shift(:,n(2));
+%yy(:,1) = y(:,1);
+yy(:,ny) = -y_shift(:,ny);
 
 y=yx+yy;
--- Functions/MyProjectionTV.m
+++ Functions/MyProjectionTV.m
@@ -5,18 +5,20 @@ pn=zeros(nx,ny,3);
 div_pn=zeros(nx,ny);
 b=pn;
 
+a2 = zeros(nx, ny, 3);
 
 for i=1:iter
 
-    a=MyTV3D_conv(div_pn-g./lam);
+    a = Grad(div_pn-g./lam);
+    [a2(:,:,1), a2(:,:,2)] = gradient(div_pn - g./lam);
 
     b(:,:,1)=sqrt(a(:,:,1).^2+a(:,:,2).^2);
     b(:,:,2)=b(:,:,1);
     b(:,:,3)=b(:,:,1);
     pn=(pn+tau.*a)./(1.0+tau.*b);
 
-
     div_pn=MyDiv3D(pn);
+    
 end
 
 p=lam.*MyDiv3D(pn);
