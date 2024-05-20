4f278815221955b626221dd2ca31d111584350a1
Ralf Mouthaan <rpm40@cam.ac.uk>
2024-05-20 21:54:57 +0930

    Some parameters renamed
    To remove ambiguities

--- CSmain.m
+++ CSmain.m
@@ -13,22 +13,30 @@
 close all; clear variables; clc;
 addpath('./Functions');
 
-%% User-Defined Parameters
+%% Parameters
 
-nx = 500; ny = 500;
+% Parameters defining geometry
+% Units are in um.
+Nx = 500; Ny = 500;
 lambda = 0.532;  % wavelength (um)
 z = 12000;  % distance from detector to first reconstructed plane (um)
 dx = 4; % um
 
+% Parameters for TwIST algorithm
+tau = 0.005; 
+tolA = 1e-6;
+iterations = 200;
+
+% Derived Parameters
 k0 = 1/lambda;
-x = (-nx/2:nx/2-1)*dx;
-kx = (-nx/2:nx/2-1)/(nx*dx);
+x = (-Nx/2:Nx/2-1)*dx;
+kx = (-Nx/2:Nx/2-1)/(Nx*dx);
 
 %% Read in image
 
 f = imread('cell.jpg');
 f = rgb2gray(f);
-f = imresize(f, [nx, ny]);
+f = imresize(f, [Nx, Ny]);
 f = 1 - im2double(f);
 
 figure;
@@ -50,7 +58,7 @@ drawnow;
 %% Field measurement and backpropagation
 
  % Backpropagation of illumination
-E=ones(nx,ny); 
+E=ones(Nx,Ny); 
 E=fftshift(fft2(E));
 E=E.*conj(Phase);
 E=ifft2(ifftshift(E));
@@ -67,18 +75,14 @@ figure;
 imshow(abs(g));
 title('Diffracted field')
 
-g = C2V(g, nx, ny);
+g = C2V(g, Nx, Ny);
 
 %% Function Definitions
 
-tau = 0.005; 
-tolA = 1e-6;
-iterations = 200;
-
-A = @(fimg) ForwardPropagation(fimg,E,nx,ny,Phase);  % forward propagation operator
-AT = @(fimg) BackwardPropagation(fimg,E,nx,ny,Phase);  % backward propagation operator
-Psi = @(f, lambda) Denoise(f, lambda, nx, ny);
-Phi = @(f) TotalVariance(f, nx, ny);
+A = @(fimg) ForwardPropagation(fimg,E,Nx,Ny,Phase);  % forward propagation operator
+AT = @(fimg) BackwardPropagation(fimg,E,Nx,Ny,Phase);  % backward propagation operator
+Psi = @(f, lam) Denoise(f, lam, Nx, Ny);
+Phi = @(f) TotalVariance(f, Nx, Ny);
 
 %% TwIST algorithm
 
@@ -88,12 +92,12 @@ f_reconstruct = ...
     'MaxIterA', iterations, 'MinIterA', iterations, ...
     'ToleranceA', tolA, 'Verbose', 1);
 
-f_reconstruct = V2C(f_reconstruct, nx, ny);
+f_reconstruct = V2C(f_reconstruct, Nx, Ny);
 
 figure;
 imshow(abs(f_reconstruct))
 
-fprintf('Err = %0.5f\n', sum(sum(abs(abs(f) - abs(f_reconstruct))))/nx/ny);
+fprintf('Err = %0.5f\n', sum(sum(abs(abs(f) - abs(f_reconstruct))))/Nx/Ny);
 
 %% Functions
 
@@ -104,18 +108,18 @@ function Kernel = GenerateKernel_ASM(k0, kx, z)
     Kernel = exp(1i*2*pi*z*sqrt(term));
 
 end
-function F = V2C(F, nx, ny)
+function F = V2C(F, Nx, Ny)
 
-    assert(length(F) == nx*ny*2);
+    assert(length(F) == Nx*Ny*2);
 
-    F = F(1:nx*ny) + 1i*F(nx*ny+1:nx*ny*2);
-    F = reshape(F, nx, ny);
+    F = F(1:Nx*Ny) + 1i*F(Nx*Ny+1:Nx*Ny*2);
+    F = reshape(F, Nx, Ny);
 
 end
-function F = C2V(F, nx, ny)
+function F = C2V(F, Nx, Ny)
 
-    assert(size(F, 1) == nx);
-    assert(size(F, 2) == ny);
+    assert(size(F, 1) == Nx);
+    assert(size(F, 2) == Ny);
 
     F = F(:);
     F=[real(F); imag(F)];
@@ -146,26 +150,26 @@ function F = BackwardPropagation(F, E, Nx, Ny, Kernel)
     F = C2V(F, Nx, Ny);
 
 end
-function RetVal = TotalVariance(F, nx, ny)
+function RetVal = TotalVariance(F, Nx, Ny)
 
-    F=reshape(F,nx,2*ny);
+    F=reshape(F,Nx,2*Ny);
     RetVal = Grad(F);
     RetVal=sum(abs(RetVal(:)));
 
 end
-function F = Denoise(F, lambda, nx, ny)
+function F = Denoise(F, lambda, Nx, Ny)
 
     % This is a denoising algorithm. I do not know where it comes from or why
     % it works, but it does seem to work. Something to do with Rudin–Osher–Fatemi?
     
-    F = reshape(F,nx,2*ny);
+    F = reshape(F,Nx,2*Ny);
     lambda = lambda*0.5;
     tau = 0.05; % Note, this tau is different in value to the one defined in the main script.
                  % But, I don't know if they have the same significance.
     
-    pn = zeros(nx,2*ny,2);
-    div_pn = zeros(nx,2*ny);
-    b = zeros(nx, 2*ny);
+    pn = zeros(Nx,2*Ny,2);
+    div_pn = zeros(Nx,2*Ny);
+    b = zeros(Nx, 2*Ny);
     
     for i=1:4
     
@@ -179,7 +183,7 @@ function F = Denoise(F, lambda, nx, ny)
     
     F = F - lambda.*div_pn;
     
-    F = reshape(F,2*nx*ny,1);
+    F = reshape(F,2*Nx*Ny,1);
 
 end
 function RetVal = Grad(F)
